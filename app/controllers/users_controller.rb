be8a1753abe30760136896a2382d9436e796ace4
makoto hata <makotohata0429@gmail.com>
2023-12-07 15:01:43 +0900

    add user create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-
+      redirect_to @user # redirect_to user_url(@user)と同じ。勝手に解釈
+      # 本来 redirect_to user_url(@user.id)と書くところだが、モデルオブジェクトが渡されるとidを取得しようとしてくれるため、@userでも、@user.idと同じ結果になる。
     else
       render 'new', status: :unprocessable_entity
     end
