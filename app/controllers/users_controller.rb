a7a36ca75f215aaa185bc5e98a5bee23c4e1ebc4
makoto hata <makotohata0429@gmail.com>
2023-12-07 15:49:20 +0900

    add flash message at signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user # redirect_to user_url(@user)と同じ。勝手に解釈
       # 本来 redirect_to user_url(@user.id)と書くところだが、モデルオブジェクトが渡されるとidを取得しようとしてくれるため、@userでも、@user.idと同じ結果になる。
     else
