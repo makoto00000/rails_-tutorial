7fa33ec4af912864e743e627b1158b2d3ea07188
makoto hata <makotohata0429@gmail.com>
2023-12-08 16:37:51 +0900

    refuctor menu.js

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -1,15 +1,13 @@
-document.addEventListener("turbo:load", function() {
-  let hamburger = document.querySelector("#hamburger");
-  hamburger.addEventListener("click", function(event) {
+function addToggleListener(selected_id, menu_id, toggle_class) {
+  let selected_element = document.querySelector(`#${selected_id}`);
+  selected_element.addEventListener("click", function(event) {
     event.preventDefault();
-    let menu = document.querySelector("#navbar-menu");
-    menu.classList.toggle("collapse");
+    let menu = document.querySelector(`#${menu_id}`);
+    menu.classList.toggle(toggle_class);
   });
+}
 
-  let account = document.querySelector("#account");
-  account.addEventListener("click", function(event) {
-    event.preventDefault();
-    let menu = document.querySelector("#dropdown-menu");
-    menu.classList.toggle("active");
-  });
+document.addEventListener("turbo:load", function() {
+  addToggleListener("hamburger", "navbar-menu", "collapse")
+  addToggleListener("account", "dropdown-menu", "active")
 });
