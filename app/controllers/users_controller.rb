a331a997dff64ae741287cf8b1ba0e51a8e8825e
makoto hata <makotohata0429@gmail.com>
2023-12-10 20:19:28 +0900

    before_action logged_in_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
   def new
     @user = User.new
   end
@@ -39,5 +40,12 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_path, status: :see_other
+    end
   end
 end
