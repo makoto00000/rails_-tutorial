a2f1c1a0643d0b113375a4c6ee59cce247988b87
makoto hata <makotohata0429@gmail.com>
2023-12-10 17:03:30 +0900

    Measures against session replay attacks

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,6 +2,7 @@ module SessionsHelper
 
   def log_in(user)
     session[:user_id] = user.id
+    session[:session_token] = user.session_token
   end
 
   def remember(user)
@@ -10,9 +11,12 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
-  def current_user
+  def current_user # rubocop:disable Metrics/MethodLength
     if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
+      user = User.find_by(id: user_id)
+      if user && session[:session_token] == user.session_token
+        @current_user = user
+      end
     elsif (user_id = cookies.encrypted[:user_id])
       # テストコードを書き忘れていると通ってしまう
       # raise "untested error"
